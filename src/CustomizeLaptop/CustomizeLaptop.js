64cbe9fbdc0e6f7d784af22f86f21a6b75e373c4
Adam Pham <adammtpham@gmail.com>
2020-11-23 12:22:05 -0600

    factored ItemOptions into ListItems

--- src/CustomizeLaptop/CustomizeLaptop.js
+++ src/CustomizeLaptop/CustomizeLaptop.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react';
 import ListItems from '../ListItems/ListItems';
-import ItemOptions from '../ItemOptions/ItemOptions';
-import slugify from 'slugify';
 
 class CustomizeLaptop extends Component {
     render() {
         const features = Object.keys(this.props.features).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
-            const options = this.props.features[feature].map(item => {
-                const itemHash = slugify(JSON.stringify(item));
-                return (
-                    <ItemOptions 
-                        selected={this.props.selected}
-                        itemHash={itemHash}
-                        feature={feature}
-                        item={item}
-                        key={item.name}
-                        updateFeature={this.props.updateFeature}
-                    />
-                )
-            });
+            
             return (
                 <ListItems 
                     key={idx}
                     feature={feature}
-                    options={options}
+                    options={this.props.features[feature]}
                     featureHash={featureHash}
+                    updateFeature={this.props.updateFeature}
+                    selected={this.props.selected}
                 />
             );
         });
--- src/ListItems/ListItems.js
+++ src/ListItems/ListItems.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import slugify from 'slugify';
+import ItemOptions from '../ItemOptions/ItemOptions'
+
 
 class ListItems extends Component {
     render() {
+        const options = this.props.options.map(item => {
+            const itemHash = slugify(JSON.stringify(item));
+            return (
+                <ItemOptions 
+                    selected={this.props.selected}
+                    itemHash={itemHash}
+                    feature={this.props.feature}
+                    item={item}
+                    key={item.name}
+                    updateFeature={this.props.updateFeature}
+                />
+            )
+        });
         return (
             <fieldset className="feature" key={this.props.featureHash}>
                 <legend className="feature__name">
                     <h3>{this.props.feature}</h3>
                 </legend>
-                {this.props.options}
+                {options}
             </fieldset>
         )
     }
